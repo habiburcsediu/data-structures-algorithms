33a4297fbd5b2e226a18cf771a4490e881252f35
Md Habibur Rahman <habibur.cse.diu@gmail.com>
2025-03-29 23:10:27 +0600

    feat: implement optimized selection sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -29,3 +29,21 @@ def selection_sort(arr: list[int]) -> list[int]:
     return arr
 
 print(selection_sort([3, 7, 9, 11, 12]))
+
+# We can optimize selection sort algorithm to use swapping insted of all shifting
+def optimized_selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i] # Swap the minimum element with the first element of the list in every iteration
+
+    return arr
+
+print(optimized_selection_sort([3, 7, 9, 11, 12]))
+
