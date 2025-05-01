6e395bb6a870c7a88fd3386f6a3f1566e4d786f6
Md Habibur Rahman <habibur.cse.diu@gmail.com>
2025-05-01 23:06:01 +0600

    feat(array): implement binary search algorithm recursively

--- searching_array.py
+++ searching_array.py
@@ -12,3 +12,31 @@ if result != -1:
     print("Found at index:", result)
 else:
     print("Not Found!")
+
+
+def binary_search_recursively(arr, target, low, high):
+    if low > high:
+        return -1 # Target is not found
+    
+    mid = (low + high) // 2
+    
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search_recursively(arr, target, low, mid - 1)
+    else:
+        return binary_search_recursively(arr, target, mid + 1, high)
+    
+
+arr = [1, 2, 3, 4, 5]
+target = 6
+low = 0
+high = len(arr) - 1
+
+result = binary_search_recursively(arr, target, low, high)
+if result != -1:
+    print("Found at index:", result)
+else:
+    print("Not found!")
+
+
