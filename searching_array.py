47423d52aa1b7052fffac26a652010ba7337a027
Md Habibur Rahman <habibur.cse.diu@gmail.com>
2025-05-02 23:50:18 +0600

    feat(array): implement binary search algorithm iteratively

--- searching_array.py
+++ searching_array.py
@@ -15,6 +15,7 @@ else:
 
 
 def binary_search_recursively(arr, target, low, high):
+    """Search an element from an array using binary search algorithm recursively"""
     if low > high:
         return -1 # Target is not found
     
@@ -40,3 +41,28 @@ else:
     print("Not found!")
 
 
+def binary_search_iteratively(arr: list[int], target: int, low: int, high: int) -> int:
+    """Search an element from an array using binary search algorithm iteratively"""
+    
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -1
+    
+arr = [1, 2, 3, 4, 5]
+target = 4
+low = 0
+high = len(arr) - 1
+
+result = binary_search_iteratively(arr, target, low, high)
+if result != -1:
+    print("Found at index:", result)
+else:
+    print("Not found!")
